080530994fb2f39db02f1a8d405756aec41b10bb
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2016-03-18 16:13:58 +0100

    add support for compiling tulip.js to experimental WebAssembly (only works in Chrome Canary for the moment, Firefox nightly fails to validate the generated wasm file) + remove tulip-lite compilation and tulip_webix demo

--- src/emscripten/wrap_compiled_code.cmake
+++ src/emscripten/wrap_compiled_code.cmake
@@ -5,17 +5,35 @@ FILE(WRITE ${JSFILE} "!function() {")
 FILE(APPEND ${JSFILE} "  ${JSUTILS_CODE}")
 FILE(APPEND ${JSFILE} "  ${EMSCRIPTEN_GENERATED_CODE}")
 FILE(APPEND ${JSFILE} "  var tulip = {};")
+FILE(APPEND ${JSFILE} "  this.tulip = tulip;")
 FILE(APPEND ${JSFILE} "  var scriptName = getScriptName();")
 FILE(APPEND ${JSFILE} "  var scriptPath = getScriptPath();")
 FILE(APPEND ${JSFILE} "  tulip.filePackagePrefixURL = scriptPath;")
 FILE(APPEND ${JSFILE} "  tulip.memoryInitializerPrefixURL = scriptPath;")
 IF(TULIP_VIZ_FEATURES)
-FILE(APPEND ${JSFILE} "  tulip.vizFeatures = true;")
+  FILE(APPEND ${JSFILE} "  tulip.vizFeatures = true;")
 ELSE(TULIP_VIZ_FEATURES)
-FILE(APPEND ${JSFILE} "  tulip.vizFeatures = false;")
+  FILE(APPEND ${JSFILE} "  tulip.vizFeatures = false;")
 ENDIF(TULIP_VIZ_FEATURES)
-FILE(APPEND ${JSFILE} "  tulip = tulipjs(tulip);")
-FILE(APPEND ${JSFILE} "  if (typeof define === 'function' && define.amd) define(tulip); else if (typeof module === 'object' && module.exports) module.exports = tulip;")
-FILE(APPEND ${JSFILE} "  this.tulip = tulip;")
+IF(USE_WASM)
+  FILE(APPEND ${JSFILE} "  var xhr = new XMLHttpRequest();")
+  FILE(APPEND ${JSFILE} "  xhr.open('GET', scriptPath + 'tulip.wasm');")
+  FILE(APPEND ${JSFILE} "  xhr.responseType = 'arraybuffer';")
+  FILE(APPEND ${JSFILE} "  xhr.onload = function() {")
+  FILE(APPEND ${JSFILE} "    tulip.wasmBinary = xhr.response;")
+ENDIF(USE_WASM)
+FILE(APPEND ${JSFILE} "    tulip = tulipjs(tulip);")
+FILE(APPEND ${JSFILE} "    if (typeof define === 'function' && define.amd) define(tulip); else if (typeof module === 'object' && module.exports) module.exports = tulip;")
+IF(USE_WASM)
+  FILE(APPEND ${JSFILE} "  };")
+  FILE(APPEND ${JSFILE} "  xhr.send(null);")
+ENDIF(USE_WASM)
 FILE(APPEND ${JSFILE} "}();")
+
+
+
+
+
+
+
 
